28bed8f2d4d967b757a386f363cdbbdc706d5b60
LaurenMartini <laurenmartini55@gmail.com>
2021-04-08 22:07:38 -0700

    updated body-parser to express per mentor

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 //app to use cors for cross origin allowance
 app.use(cors());
 
-//use body-parser
+//use body-parser (updated per https://knowledge.udacity.com/questions/509946 mentor response)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //create server
 const port = 8000;
